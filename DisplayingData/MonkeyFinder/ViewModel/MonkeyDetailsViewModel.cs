a940ecbc826ca28c39785011368be5ec7e8e0f2d
Angel J. Haro <haro.j.angel@gmail.com>
2025-03-17 07:43:56 -0700

    viewmodel

--- DisplayingData/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ DisplayingData/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,5 +1,18 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
+[QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+  public MonkeyDetailsViewModel()
+  {
+  }
+  
+  [ObservableProperty]
+  Monkey monkey;
+
+// -- THIS IS HOW WE CAN DO MODAL IN IOS 
+  // [RelayCommand]
+  // async Task GoBackAsync(){
+  //   await Shell.Current.GoToAsync("..");
+  // }
 }
--- DisplayingData/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ DisplayingData/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -7,14 +7,76 @@ public partial class MonkeysViewModel : BaseViewModel
   MonkeyService monkeyService;
 
   public ObservableCollection<Monkey> Monkeys { get; } = new();
-  
+
   // public Command GetMonkeysCommand {get;} -- toolkit 
 
-  public MonkeysViewModel(MonkeyService monkeyService)
+  IConnectivity connectivity;
+  IGeolocation geolocation;
+
+  public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
   {
     Title = "Monkey Finder";
     this.monkeyService = monkeyService;
     // GetMonkeysCommand = new Command(async ()=> await GetMonkeysAsync()); -- toolkit
+    this.connectivity = connectivity;
+    this.geolocation = geolocation;
+  }
+
+  [RelayCommand]
+  async Task GetClosestMonkeyAsync()
+  {
+    if (IsBusy || Monkeys.Count == 0)
+      return;
+
+    try
+    {
+      var location = await geolocation.GetLastKnownLocationAsync();
+      if (location == null)
+      {
+        location = await geolocation.GetLocationAsync(
+          new GeolocationRequest
+          {
+            DesiredAccuracy = GeolocationAccuracy.Medium,
+            Timeout = TimeSpan.FromSeconds(30),
+          });
+      }
+
+      if (location == null)
+      {
+        return;
+      }
+      var first = Monkeys.OrderBy(m =>
+      location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)
+      ).FirstOrDefault();
+
+      if (first == null)
+        return;
+
+      await Shell.Current.DisplayAlert("Closest Monkey",
+      $"{first.Name} in {first.Location}", "OK");
+    }
+    catch (Exception ex)
+    {
+      Debug.WriteLine(ex);
+      await Shell.Current.DisplayAlert("Error! ",
+      $"Unable to get closest monkey: {ex.Message}", "OK");
+    }
+
+  }
+
+  [RelayCommand]
+  async Task GoToDetailsAsync(Monkey monkey)
+  {
+    if (monkey is null)
+    {
+      return;
+    }
+    await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
+    new Dictionary<string, object>
+    {
+      {"Monkey", monkey}
+    });
+
   }
 
   // [RelayCommand] --> [ICommand] (decapricated)
@@ -24,26 +86,30 @@ public partial class MonkeysViewModel : BaseViewModel
     if (IsBusy)
       return;
 
-      try
+    try
+    {
+      if (connectivity.NetworkAccess != NetworkAccess.Internet)
       {
-        IsBusy = true;
-        var monkeys= await monkeyService.GetMonkeys();
+        await Shell.Current.DisplayAlert("Internet issue", $"Check your internet and try again!", "OK");
+        return;
+      }
+      IsBusy = true;
+      var monkeys = await monkeyService.GetMonkeys();
 
-        if (Monkeys.Count != 0 )
-          Monkeys.Clear();
+      if (Monkeys.Count != 0)
+        Monkeys.Clear();
 
-        foreach (var monkey in monkeys)
-          Monkeys.Add(monkey);
-      }
-      catch (Exception ex)
-      {
-        Debug.WriteLine(ex);
-        await Shell.Current.DisplayAlert("Error! ", $"Unable to get monkeys: {ex.Message}", "OK");
-        
-      }
-      finally
-      {
-        IsBusy= false;
-      }
+      foreach (var monkey in monkeys)
+        Monkeys.Add(monkey);
+    }
+    catch (Exception ex)
+    {
+      Debug.WriteLine(ex);
+      await Shell.Current.DisplayAlert("Error! ", $"Unable to get monkeys: {ex.Message}", "OK");
+    }
+    finally
+    {
+      IsBusy = false;
+    }
   }
 }
